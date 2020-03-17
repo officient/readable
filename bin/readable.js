d9620288d43cab7631c14647bc72270bfa81db7d
Denys Potapov <denys.potapov@gmail.com>
2020-03-17 17:19:47 +0200

    add main cli

---
+++ bin/readable.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+/**
+ * @fileoverview Main CLI that is run via the readable command.
+ * @author Denys Potapo
+ */
+
+/* eslint no-console:off */
+
+"use strict";
+
+console.log('test');
