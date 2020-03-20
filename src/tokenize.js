e1bb4b99c550e4f0b7176a2dc90a7ebccad99d0d
Denys Potapov <denys.potapov@gmail.com>
2020-03-20 14:31:13 +0200

    helpers in tokens

--- src/tokenize.js
+++ src/tokenize.js
@@ -2,9 +2,9 @@ const Stream = require('./stream');
 
 const types = {
   whitespace: 'whitespace',
-  newline: 'newline',
   comment: 'comment',
   other: 'other',
+  eof: 'eof',
 };
 
 // wraper around array of tokens
@@ -12,13 +12,48 @@ const types = {
 class Tokens {
   constructor(tokens) {
     this.array = tokens;
+    this.pos = 0;
+  }
+
+  prev() { this.pos -= 1; }
+
+  next() { this.pos += 1; }
+
+  body() { return this.current().body; }
+
+  type() { return this.current().type; }
+
+  current() {
+    if ((this.pos < 0) || (this.pos >= this.array.length)) {
+      return { body: '', type: types.eof };
+    }
+
+    return this.array[this.pos];
+  }
+
+  forEach(callback) {
+    const { pos } = this;
+    for (let i = 0; i < 9; i += 1) {
+      this.pos = i;
+      callback(this);
+    }
+    // keep position
+    this.pos = pos;
+  }
+
+  find(str, callback) {
+    this.forEach((tokens) => {
+      if (tokens.current().body === str) {
+        callback(tokens);
+      }
+    });
   }
 }
 
 
 function readToken(stream) {
   if (stream.eat('\r') || stream.eat('\n')) {
-    return types.newline;
+    return types.whitespace;
   }
   if (stream.eat(/\s+/)) {
     return types.whitespace;
