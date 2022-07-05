ff34ef842d0f6c413bf755fcfe5e4e6cd682c86b
Jarre Knockaert <jarre@officient.io>
2022-07-05 12:10:32 +0200

    fix comment rule

--- src/line-count.js
+++ src/line-count.js
@@ -6,30 +6,37 @@ function countLines(startToken, endToken, { comments, emptyLines }) {
   // Count empty lines
   let typesPerLine = {};
   const tokenIterator = startToken.copy();
-  while (tokenIterator.pos !== endToken.pos) {
-    const currentLine = tokenIterator.current().line;
+  let emptyLineCount = 0;
+  let previousToken = null;
+  let currentToken = tokenIterator.current();
+  while (tokenIterator.pos <= endToken.pos && tokens.type() !== types.eof) {
+    const currentLine = currentToken.line;
+
+    // Update types per line
     const group = (typesPerLine[currentLine] || []);
     group.push(tokenIterator.current().type);
     typesPerLine = { ...typesPerLine, [currentLine]: group };
-    tokenIterator.step(false, true);
-  }
 
-  let emptyLineCount = Object.values(typesPerLine)
-    .filter((typesOnLine) => typesOnLine.every((tokenType) => tokenType === types.whitespace))
-    .length;
-
-  // Ignore new lines after block comments
-  const lineNumbers = Object.keys(typesPerLine).sort();
-  for (let i = 1; i < lineNumbers.length; i += 1) {
-    const previousLine = typesPerLine[lineNumbers[i - 1]];
-    const currentLine = typesPerLine[lineNumbers[i]];
-    const prevLineIsComment = previousLine.every((tokenType) => tokenType === types.comment);
-    const curLineIsWhitespace = currentLine.every((tokenType) => tokenType === types.whitespace);
-    // Current line contains the new line for the block comment. So, this is not an empty line.
-    if (prevLineIsComment && curLineIsWhitespace) {
+    // If a comment is followed with a newline on a future line number.
+    // The comment ends on that line. Therefore, the future line is not an empty line.
+    // And we cannot count this for our empty line count.
+    const isDiffLine = previousToken && previousToken.line !== currentToken.line;
+    const currentIsNewLine = currentToken.body === '\n' || currentToken.body === '\r';
+    const previousIsComment = previousToken && previousToken.type === types.comment;
+    if (isDiffLine && previousIsComment && currentIsNewLine) {
       emptyLineCount -= 1;
     }
+
+    // Step through iterator and update variables
+    tokenIterator.step(false, true);
+    previousToken = currentToken;
+    currentToken = tokenIterator.current();
   }
+
+  // Empty lines are lines which contain only whitespaces
+  emptyLineCount += Object.values(typesPerLine)
+    .filter((typesOnLine) => typesOnLine.every((tokenType) => tokenType === types.whitespace))
+    .length;
 
   // Count lines including empty lines. Excludes comments if `include-comments` is falsy.
   const lines = new Set();
